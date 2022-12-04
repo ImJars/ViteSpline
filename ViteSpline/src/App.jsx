7f954bc8110cf3062c3cf4f0e9a37206116756d6
ImJars <ra374885@uaeh.edu.mx>
2022-12-03 22:44:38 -0600

    Add styled-components & responsive-example

--- ViteSpline/src/App.jsx
+++ ViteSpline/src/App.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 20px;
+`
+const Text = styled.h1`
+  font-size: clamp(0.75rem, 0.0909rem + 4.2182vw, 8rem);
+`
 function App() {
   return (
     <>
@@ -7,12 +16,18 @@ function App() {
         <header
           className='w-full h-16 bg-red-900 grid justify-center content-center'
         >
-          <h1
-            className='text-4xl font-bold text-white'
+          <Text
+            className='text-xs font-bold text-white'
           >
             Hello! this My App
-          </h1>
+          </Text>
         </header>
+        <Container>
+          <div className='bg-red-500 h-64 m-2'>1</div>
+          <div className='bg-red-500 h-64 m-2'>2</div>
+          <div className='bg-red-500 h-64 m-2'>3</div>
+          <div className='bg-red-500 h-64 m-2'>4</div>
+        </Container>
       </section>
     </>
   )
