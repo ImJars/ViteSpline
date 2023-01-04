3919728d44963f67a2e22935849e99aeb2d1786a
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-01-03 23:52:48 -0600

    Add idea_header

---
+++ ViteSpline/src/header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <>
+            <header>
+                <div>
+
+                </div>
+                <div>
+                    <ul>
+                        
+                    </ul>
+                </div>
+            </header>
+        </>
+    )
+}
+
+export default Header
