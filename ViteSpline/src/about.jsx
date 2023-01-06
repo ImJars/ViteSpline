7dce697b33c1a11699fb2dad09ff15b4df76b89c
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-01-05 23:59:37 -0600

    Add About_section

---
+++ ViteSpline/src/about.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function About() {
+    return (
+        <>
+            <section
+                className='w-full h-screen flex justify-center 
+                        items-center bg-background'
+            >
+                <h1
+                    className='text-7xl font-bold'
+                >
+                    About Me
+                </h1>
+            </section>
+        </>
+    )
+}
+
+export default About
