efbe1727258fa4c685afcdbfc364867df26ead4f
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-01-09 22:47:14 -0600

    Update Menu_component

--- ViteSpline/src/components/menu_component.jsx
+++ ViteSpline/src/components/menu_component.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Menu_bar = styled.div`
+    width: 50px;
+    height: 5px;
+    background-color: black;
+    margin: 5px 0;
+    border-radius: 50px;
+`
 function Menu_component() {
-  return (
-    <div>Menu_component</div>
-  )
+    return (
+        <section
+            className='flex flex-col'
+        >
+            <Menu_bar />
+            <Menu_bar />
+            <Menu_bar />
+            <Menu_bar />
+            <Menu_bar />
+        </section>
+    )
 }
 
 export default Menu_component
--- ViteSpline/src/header.jsx
+++ ViteSpline/src/header.jsx
@@ -1,58 +1,14 @@
 import React from 'react'
 import Header_link from './components/header_link'
+import Menu_component from './components/menu_component'
 
 function Header() {
     return (
         <>
             <header
-                className='fixed bg-background w-full flex justify-between items-center px-20 py-5 z-10'
+                className='fixed z-10'
             >
-                <div>
-                    <a 
-                        href="https://github.com/ImJars"
-                        target='_blank'
-                        rel='noreferrer'
-                    >
-                        <h1
-                            className='text-2xl font-bold tracking-wider
-                                    text-color_dark_blue'
-                        >
-                            Jars.
-                        </h1>
-                    </a>
-                </div>
-                <div>
-                    <ul
-                        className='flex space-x-16'
-                    >
-                        <Header_link 
-                            name_link='Home'
-                            section_link='#'
-                        />
-                        <Header_link
-                            name_link='About'
-                            section_link='#'
-                        />
-                        <Header_link
-                            name_link='Services'
-                            section_link='#'
-                        />
-                        <Header_link
-                            name_link='Portfolio'
-                            section_link='#'
-                        />
-                        <Header_link
-                            name_link='Blog'
-                            section_link='#'
-                        />
-                        <Header_link
-                            classNames={'bg-color_dark_blue px-6 py-2 rounded-md text-white'}
-                            name_link='Contact'
-                            section_link='#'
-                        />
-                        
-                    </ul>
-                </div>
+                <Menu_component />
             </header>
         </>
     )
