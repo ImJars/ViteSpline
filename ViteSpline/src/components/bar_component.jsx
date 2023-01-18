cfd87939e9259d05da49282ff0b83daa13519e77
Angel Ramirez <ra374885@uaeh.edu.mx>
2023-01-17 19:39:47 -0600

    Add Menu-component

--- ViteSpline/src/components/bar_component.jsx
+++ ViteSpline/src/components/bar_component.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
 
-const Menu_bar = styled.button`
-    
-`
 function Bar_component({ link, nameLink }) {
-    
     return (
-      <>
-        <Menu_bar
-        >
-          <a 
-            href={ link }
-            className='bar'>
-              <h1 className='namelink'>
-                { nameLink }
-              </h1>
-          </a>  
-        </Menu_bar>
-      </>
-    )
+        <>
+            <a href={ link } className="group py-3 px-6 block relative">
+                <div
+                    className="w-7 border-color_dark_blue h-0.5 
+                            border-2 rounded-full transition-all 
+                            duration-500 group-hover:w-16 
+                            group-hover:border-color_orange"
+                >
+                    <div
+                        className="bg-themeDarkBlue bg-opacity-50 
+                                rounded-full backdrop-blur p-2 absolute 
+                                top-1/2 -translate-y-1/2 -translate-x-full 
+                                opacity-0 group-hover:-translate-x-[115%] 
+                                transition-all duration-700 group-hover:opacity-100"
+                    >
+                        <span
+                            className="tracking-[.25em] text-xs text-themePurple 
+                                    whitespace-nowrap transition-all duration-500"
+                        >
+                        { nameLink }
+                        </span>
+                    </div>
+                </div>
+            </a>
+        </>
+    );
 }
 
-export default Bar_component
+export default Bar_component;
--- ViteSpline/src/components/menu_component.jsx
+++ ViteSpline/src/components/menu_component.jsx
@@ -1,41 +1,36 @@
-import React from 'react'
+import React from "react";
+import Bar_component from "./bar_component";
 
 function Menu_component() {
     return (
         <>
-            <nav
-                className='fixed transform -translate-y-1/2 right-5 
-                        sm:top-1/2 w-24 top-1/2 hidden sm:block z-50'
-            >
-                <a 
-                    href="#"
-                    className='group py-3 px-6 block relative'
-                >
-                    <div
-                        className='w-16 border-themePurple h-0.5 
-                                border-2 rounded-full transition-all 
-                                duration-500 group-hover:w-16 
-                                group-hover:border-color_orange'
-                    >
-                        <div
-                            className='bg-themeDarkBlue bg-opacity-50 
-                                    rounded-full backdrop-blur p-2 absolute 
-                                    top-1/2 -translate-y-1/2 -translate-x-full 
-                                    opacity-0 group-hover:-translate-x-[115%] 
-                                    transition-all duration-700 group-hover:opacity-100'
-                        >
-                            <span
-                                className='tracking-[.25em] text-xs text-themePurple 
-                                        whitespace-nowrap transition-all duration-500'
-                            >
-                                HOME
-                            </span>
-                        </div>
-                    </div>
-                </a>
-            </nav>
+        <nav
+            className="fixed transform -translate-y-1/2 right-5 
+                            sm:top-1/2 w-24 top-1/2 hidden sm:block z-50"
+        >
+            <Bar_component 
+                link="#home"
+                nameLink="HOME"
+            />
+            <Bar_component
+                link="#about"
+                nameLink="ABOUT"
+            />
+            <Bar_component
+                link="#skills"
+                nameLink="SKILLS"
+            />
+            <Bar_component
+                link="#projects"
+                nameLink="PROJECTS"
+            />
+            <Bar_component
+                link="#contact"
+                nameLink="CONTACT"
+            />
+        </nav>
         </>
-    )
+    );
 }
 
-export default Menu_component
+export default Menu_component;
